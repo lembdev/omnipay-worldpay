9298202861348edaf5e35d88355195094e4408b5
Alexander Vyzhanov <lembdev@gmail.com>
2017-04-27 12:19:47 +0300

    Update CardDelete documentation

--- src/Message/CardDeleteRequest.php
+++ src/Message/CardDeleteRequest.php
@@ -6,6 +6,34 @@
 
 namespace lembdev\WorldPay\Message;
 
+/**
+ * WorldPay Delete Credit Card Request.
+ *
+ * ### Example
+ *
+ * ```php
+ *   // Create a gateway for the WorldPay Gateway
+ *   // (routes to GatewayFactory::create)
+ *   $gateway = Omnipay::create('\\lembdev\\WorldPay\\Gateway');
+ *
+ *   // Initialise the gateway
+ *   $gateway->initialize([
+ *       'serviceKey' => 'T_S_2addbca3-d0a5-486c-9f83-3d64b6c73288',
+ *       'clientKey'  => 'T_C_c27428cd-9005-4dd3-8f7e-734c06a79abd',
+ *   ]);
+ *
+ *   // Do a create card transaction on the gateway
+ *   $response = $gateway->deleteCard([
+ *       'token' => 'CARD_TOKEN',
+ *   ])->send();
+ *
+ *   if ($response->isSuccessful()) {
+ *       echo "Gateway deleteCard was successful.\n";
+ *   }
+ * ```
+ *
+ * @method CardDeleteResponse send()
+ */
 class CardDeleteRequest extends AbstractRequest
 {
     protected $endpointUri = '/tokens';
